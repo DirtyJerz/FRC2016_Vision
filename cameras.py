67cd9b02c5b51eff627944a0f9c74c81bd7b3a41
Mike O'Connor <moconnor@oled.com>
2017-03-30 23:22:31 -0400

    Live at Montgomery Districts

--- cameras.py
+++ cameras.py
@@ -1,64 +1,52 @@
-import cv2
-import imutils
+import cv2, imutils, time
 from constants import *
+from networktables import NetworkTables
 
 def getCameras():
 	'''
 	initializes cameras 
 	'''
-	Lcap = cv2.VideoCapture(0)
-	Rcap = cv2.VideoCapture(1)
-	# Lcap=cv2.VideoCapture('http://127.0.0.1:8080/?action=stream_0&dummy=param.mjpg')
-	# Rcap=cv2.VideoCapture('http://127.0.0.1:8080/?action=stream_1&dummy=param.mjpg')
-	Lcap.set(3,WIDTH)
-	Rcap.set(3,WIDTH)
-	Lcap.set(4,HEIGHT)
-	Rcap.set(4,HEIGHT)
-	Rcap.set(cv2.cv.CV_CAP_PROP_EXPOSURE,EXPOSURE)
-	Lcap.set(cv2.cv.CV_CAP_PROP_EXPOSURE,EXPOSURE)
-	return Lcap, Rcap
+	cap = cv2.VideoCapture(0)
+	# cap=cv2.VideoCapture('http://127.0.0.1:8080/?action=stream_0&dummy=param.mjpg')
+	# cap.set(3,WIDTH)
+	# cap.set(4,HEIGHT)
+	# cap.set(cv2.cv.CV_CAP_PROP_EXPOSURE,EXPOSURE)
+	return cap
 
 class USBVideoCamera(object):
 	def __init__(self):
-		# Using OpenCV to capture from device 0. If you have trouble capturing
-		# from a webcam, comment the line below out and use a video file
-		# instead.
 		self.video = getCameras()
-		# If you decide to use video.mp4, you must have this file in the folder
-		# as the main.py.
-		# self.video = cv2.VideoCapture('video.mp4')
-	
+		self.sd=NetworkTables.getTable('SmartDashboard')
 	def __del__(self):
-		self.video[0].release()
-		self.video[1].release()
+		self.video.release()
 
-	def get_frame(self, idx, hsvFrames=False):
-		success, frame = self.video[idx].read()
+	def get_frame(self, idx):
+		success, frame = self.video.read()
 		height, width = frame.shape[:2]
 		destX=width/2
 		destY=height/2
 		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-		if not hsvFrames:
-			hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+		if self.sd.getString('showCalResult')=='True':
 			mask = cv2.inRange(hsv, LOWER_BLUE, UPPER_BLUE)
 			cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 			cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+			centers=[]
 			for c in cnts:
+				params={}
 				# compute the moments of the contour
 				M = cv2.moments(c)
 				#filter smaller shapes
 				if M["m00"]<50:
-					continue        		                
+					continue        		  
+				center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]), int(M["m00"]))
+				centers.append(center)
 				# draw the contour and center of the shape on the image
 				cv2.drawContours(frame, [c], -1, (0, 255, 100), 2)
-			# We are using Motion JPEG, but OpenCV defaults to capture raw images,
-			# so we must encode it into JPEG in order to correctly display the
-			# video stream.
+
 			ret, jpeg = cv2.imencode('.jpg', frame)
-			# ret, jpeg = cv2.imencode('.jpg', thresh)
 			return jpeg.tostring()
 		else: 
 			cv2.circle(frame, (destX, destY), 2, (0, 0, 255), -1)
-			# print hsv[destY, destX]
+			cv2.putText(frame, 'HSV= {}'.format(hsv[destY, destX]), (100,100), cv2.FONT_HERSHEY_SIMPLEX, 1 ,(255,255,255),2)
 			ret, jpeg = cv2.imencode('.jpg', frame)
 			return jpeg.tostring()
--- main.py
+++ main.py
@@ -1,214 +1,123 @@
+#!/usr/bin/env python
 from __future__ import division
 from constants import *
 from cameras import getCameras
+from functions import processFrame
 from networktables import NetworkTables
-import cv2, sys
-import numpy as np
-import imutils, math, time
-import RPi.GPIO as GPIO
 import subprocess as sp
+import RPi.GPIO as GPIO
 from streamServer import app
 from multiprocessing import Process
-import urllib
-
+import time, traceback
+with open('/home/pi/FRC2016_Vision/output.log', 'w'):
+	pass
+sp.Popen('/usr/bin/v4l2-ctl -c exposure_auto=1', shell=True, stdout=sp.PIPE)
+sp.Popen('/usr/bin/v4l2-ctl -c exposure_absolute=1', shell=True, stdout=sp.PIPE)
 import logging
-logging.basicConfig(level=logging.DEBUG)
-
-
-
-def processFrame(frame):
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-	mask = cv2.inRange(hsv, LOWER_BLUE, UPPER_BLUE)
-	cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-	c2X=0
-	dicts=[]
-	for c in cnts:
-		# compute the moments of the contour
-		M = cv2.moments(c)
-		#filter smaller shapes
-		if M["m00"]<50:
-			continue
-
-		params={}
-		params['leftmost'] = tuple(c[c[:,:,0].argmin()][0])
-		params['rightmost'] = tuple(c[c[:,:,0].argmax()][0])
-		params['width']=params['rightmost'][0]-params['leftmost'][0]
-		topmost = tuple(c[c[:,:,1].argmin()][0])
-		params['bottmost'] = tuple(c[c[:,:,1].argmax()][0])
-		params['cx'] = int(M["m10"] / M["m00"])
-		params['cy'] = int(M["m01"] / M["m00"])	
-		dicts.append(params)
-		cv2.drawContours(frame, [c], -1, (0, 255, 100), 2)
-
-	return dicts
+logging.basicConfig(filename='/home/pi/FRC2016_Vision/output.log',level=LEVEL)
+logger=logging.getLogger(__name__)
 
 def alignGear():
+	logger.debug('Entered Align Gear Routine')
 	GPIO.output(LED1_PIN, GPIO.HIGH)
 	GPIO.output(LED2_PIN, GPIO.HIGH)
-	Lcap, Rcap = getCameras()
-	# sd = NetworkTables.getTable('SmartDashboard')
+	time.sleep(1)
+	# video = USBVideoCamera()
 
+	video = getCameras()
+	dx=1000
+	distINCH=1000
+	theta=1000
 	while True:
-		# if not sd.getString('PIMode')=='GearAlignment':
-			# GPIO.output(LED1_PIN, GPIO.LOW)
-			# GPIO.output(LED2_PIN, GPIO.LOW)
-			# print 'Break AlignGear'
-			# break
-		# _,lFrame=Lcap.read()
-		# _,rFrame=Rcap.read()
-		lFrame=getFrame(0)
-		rFrame=getFrame(1)
+		if not sd.getString(MODE_KEY)=='GearAlignment':
+			logger.debug(sd.getString(MODE_KEY))
+			GPIO.output(LED1_PIN, GPIO.LOW)
+			GPIO.output(LED2_PIN, GPIO.LOW)
+			logger.debug('Break AlignGear')
+			video.release()
+			break
+		# logger.debug('Getting Frame')
 
-		lparams=processFrame(lFrame)
-		rparams=processFrame(rFrame)
-
-		if len(lparams) < 1 or len(rparams) < 1:
-			# no targets
-			print 'No Targets'
-			continue
-		# For straight on bot approach, targets should be equadistant from edge of frame.
-		# (Left camera [ldx]: distance of target right edge to right frame edge. Right camera [rdx]: distance of target left edge to left frame edge.)
-
-		ldx=lFrame.shape[:2][1]-lparams[-1]['rightmost']
-
-		rdx=rparams['leftmost']
-		dx=rdx-ldx
-		#write dx to network table
-		print dx
-		sd.putNumber('GearAlignCorrection', dx)
+		_, frame = video.read()
+		success, distINCH, theta, frame = processFrame(frame)
+		
+		sd.putNumber('GearAlignAngleError', theta)
+		sd.putNumber('GearAlignDistance', distINCH)
 
 def ledFlash():
-	print 'FlashLEDs'
+	logger.debug('FlashLEDs')
 	while True:
-		print 'FLASH'
+		logger.debug('FLASH')
 		GPIO.output(LED1_PIN, GPIO.HIGH)
 		GPIO.output(LED2_PIN, GPIO.LOW)
 		time.sleep(LED_FLASH_DELAY_S)
 		GPIO.output(LED1_PIN, GPIO.LOW)
 		GPIO.output(LED2_PIN, GPIO.HIGH)
 		time.sleep(LED_FLASH_DELAY_S)
-		if not sd.getString('PIMode')=='FlashLEDs':
+		if not sd.getString(MODE_KEY)=='FlashLEDs':
 			GPIO.output(LED1_PIN, GPIO.LOW)
 			GPIO.output(LED2_PIN, GPIO.LOW)
-			print 'Break FlashLEDs'
-			# break
-
-def getFrame(camera):
-	stream=urllib.urlopen('http://127.0.0.1:8080/?action=stream_{}&dummy=param.mjpg'.format(camera))
-	bytes=''
-	while True:
-	    bytes+=stream.read(1024)
-	    a = bytes.find('\xff\xd8')
-	    b = bytes.find('\xff\xd9')
-	    if a!=-1 and b!=-1:
-	        jpg = bytes[a:b+2]
-	        bytes= bytes[b+2:]
-	        i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.CV_LOAD_IMAGE_COLOR)
-	        return i
-        
-def recordVideoCV():
-	GPIO.output(LED1_PIN, GPIO.HIGH)
-	GPIO.output(LED2_PIN, GPIO.HIGH)
-	lcap=cv2.VideoCapture('http://127.0.0.1:8080/?action=stream_0&dummy=param.mjpg')
-	rcap=cv2.VideoCapture('http://127.0.0.1:8080/?action=stream_1&dummy=param.mjpg')
-	fourCC=cv2.cv.CV_FOURCC(*'MJPG')
-	ts=time.time()
-	rightOut = cv2.VideoWriter('rightCam_{}.avi'.format(ts), fourCC, 30.0, (WIDTH, HEIGHT))
-	leftOut = cv2.VideoWriter('leftCam_{}.avi'.format(ts), fourCC, 30.0, (WIDTH, HEIGHT))
-	while(True):
-		retL, lframe = lcap.read()
-		retR, rframe = rcap.read()
-		if retL==True and retR==True:
-			leftOut.write(lframe)
-			rightOut.write(rframe)
-		else:	
+			logger.debug('Break FlashLEDs')
 			break
-
-		if not sd.getString('PIMode')=='RecordVideoCV':
-			break
-
-	lcap.release()
-	rcap.release()
-	GPIO.output(LED1_PIN, GPIO.LOW)
-	GPIO.output(LED2_PIN, GPIO.LOW)
-
-def recordVideo():
-	ts=time.time()
-	GPIO.output(LED1_PIN, GPIO.HIGH)
-	GPIO.output(LED2_PIN, GPIO.HIGH)
-	pipe1=sp.Popen('avconv -f mjpeg -i "http://127.0.0.1:8080/?action=stream_0&dummy=param.mjpg" -an -vcodec mjpeg leftCam_{}.avi'.format(ts), shell=True, stdout=sp.PIPE)
-	pipe2=sp.Popen('avconv -f mjpeg -i "http://127.0.0.1:8080/?action=stream_1&dummy=param.mjpg" -an -vcodec mjpeg rightCam_{}.avi'.format(ts),shell=True, stdout=sp.PIPE)
-	# pipe2=sp.Popen(cmd2,stdout=sp.PIPE)
-	while True:
-		if not sd.getString('PIMode')=='RecordVideo':
-			pipe1.terminate()
-			pipe2.terminate()
-			break
-		time.sleep(1)
-	GPIO.output(LED1_PIN, GPIO.LOW)
-	GPIO.output(LED2_PIN, GPIO.LOW)
 
 def calStream():
-	
-	pass
-def startCameraStreams():
-	global stream
-	stream=sp.Popen("mjpg_streamer -i 'input_uvc.so -d /dev/video0 -f 30' -i 'input_uvc.so -d /dev/video1 -f 30' -o 'output_http.so -p 8080'", shell=True, stdout=sp.PIPE)
-
-def killCameraStreams():
-	global stream
-	stream.terminate()
+	def worker():
+		app.run(host='0.0.0.0', debug=True, use_reloader=False)
+	calibration=Process(target=worker)
+	calibration.start()
+	GPIO.output(LED1_PIN, GPIO.HIGH)
+	GPIO.output(LED2_PIN, GPIO.HIGH)
+	while sd.getString(MODE_KEY)=='CalibrateVision':
+		pass
+	GPIO.output(LED1_PIN, GPIO.LOW)
+	GPIO.output(LED2_PIN, GPIO.LOW)
+	calibration.terminate()
 
 def idle():
 	GPIO.output(LED1_PIN, GPIO.LOW)
 	GPIO.output(LED2_PIN, GPIO.LOW)
-	print 'Idling'
-
-def quit():
-	print 'Quit sig received'
-	global running
-	running=False
+	logger.debug('Idling')
 
 def valueChanged(table, key, value, isNew):
-	# print 'VAL CHANGED'
-	if key=='PIMode':
+	# logger.debug('VAL CHANGED: {}:{}'.format(key,value))
+	if key==MODE_KEY:
+		logger.debug('Changing MODE: {}:{}'.format(key,value))
 		modes[value]()
 
 modes={ 'GearAlignment': alignGear,
-		'FlashLEDs': ledFlash,
-		'RecordVideo': recordVideo,
-		'RecordVideoCV': recordVideoCV,
 		'CalibrateVision': calStream,
 		'Idle': idle,
-		'Quit':quit,
 	}
 
 if __name__ == '__main__':
 	try:
+		stream=None
 		running=True
 		# As a client to connect to a robot
 		NetworkTables.initialize(server='roborio-2016-frc.local')
-		# NetworkTables.initialize(server='127.0.0.1')
 		# NetworkTables.initialize(server='oconnor27')
 		time.sleep(.5)
 		sd = NetworkTables.getTable('SmartDashboard')
-		# sd.putString('PIMode', 'FlashLEDs')
-		# sd.addTableListener(valueChanged)
+		sd.addTableListener(valueChanged)
 		time.sleep(.5)
+		# sd.putString(MODE_KEY, 'CalibrateVision')
+		sd.putString(MODE_KEY, 'GearAlignment')
 		GPIO.setmode(GPIO.BOARD)
 		GPIO.setup(LED1_PIN, GPIO.OUT)
 		GPIO.setup(LED2_PIN, GPIO.OUT)
-		startCameraStreams()
-		# ledFlash()
+		GPIO.output(LED1_PIN, GPIO.LOW)
+		GPIO.output(LED2_PIN, GPIO.LOW)
+		# calStream()
 		alignGear()
-		# recordVideo()
+		# ledFlash()
+		time.sleep(2)
 		while running:
-
 			time.sleep(1)
 	except Exception as e:
-		raise e
+		# raise e
+		logger.debug(e)
+		# logger.error(e)
+
 	finally:
 		GPIO.cleanup()
-		killCameraStreams()
 
